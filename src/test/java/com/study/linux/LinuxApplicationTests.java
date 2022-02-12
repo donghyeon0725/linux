f188b349ae9d6400816801ec0a51e7dcf077a302
김동현 <donghyeon@liveklass.com>
2022-02-12 22:51:58 +0900

    Initial commit

---
+++ src/test/java/com/study/linux/LinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
